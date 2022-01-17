088eea31b66c998fcc600a7e791a357001d0f70e
David Childerhose <davidoforange@outlook.com>
2022-01-17 16:52:26 +0900

    Created button nav toggle function for mobile

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const nav = document.querySelector(".main-nav");
 const buttonToggle = document.querySelector(".toggle-button");
 
 buttonToggle.addEventListener("click", () => {
-    const visible = nav.getAttribute("data-expanded");
+    const visible = nav.getAttribute("data-visible");
     if (visible === "false") {
-        nav.setAttribute("data-expanded", true);
+        nav.setAttribute("data-visible", true);
+        buttonToggle.setAttribute("aria-expanded", true);
+        
     } else {
-        nav.setAttribute("data-expanded", false);
+        nav.setAttribute("data-visible", false);
+        buttonToggle.setAttribute("aria-expanded", false);
     }
 })
